656da5fd226a3de81871354eddf0e4537ebb2aa4
Reno Sorensen <renojames19@gmail.com>
2018-03-07 01:09:34 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,12 +12,11 @@ def monopoly
 	monopoly = {
 		railroads: {}
 	}
-
 end
 
 def monopoly_with_second_tier
 	monopoly = {
-		railroads: {}
+		railroads: {pieces: 4, names: {}, rent_in_dollars: {}}
 	}
 
 end
